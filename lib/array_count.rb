c06aa5067d2586cbe0f7693dbc3c7f542479b644
Music Meier <musicm987@gmail.com>
2020-08-19 22:48:06 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,9 +2,9 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
  i = 0
 if array[i] = String 
-total += 1
+i += 1
 end
-
+total
   end
     
 
