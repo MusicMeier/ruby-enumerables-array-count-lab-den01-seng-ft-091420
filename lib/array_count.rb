8e767332ac1faf194a0012b17b3e06aba3d5878d
Music Meier <musicm987@gmail.com>
2020-08-19 22:57:52 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,8 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
- i = 0
-if array[i] = String 
-i += 1
+
+array.count do |i|
+  array = String
 end
 
   end
@@ -10,6 +10,6 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-  array.count |empty_string|
+  array.count do |empty_string|
   
 end
